47cb509ad9682e8a2fac829bb829b9090efca128
Chris Miceli <chris@miceli.net.au>
2020-10-06 07:57:45 +1100

    Adding scopeguard to deal with removing old dirs
    Ffmpeg builds are around 50GB, this will allow for some clean up.
    
    Whilst here I also refactored the command running to a function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate git2;
 extern crate yaml_rust;
 extern crate log;
 extern crate simple_logger;
+#[macro_use(defer)] extern crate scopeguard;
 
 use std::path::Path;
 use simple_logger::SimpleLogger;
@@ -82,6 +83,37 @@ fn submit_results(config: &Yaml, fate_results: &HashMap<String, FateResult>, com
     std::fs::write(&result_file, resulting_string)
 }
 
+fn run_commands(run_dir: &str, config: &Yaml, commit_hash: &git2::Oid) -> Result<(), std::io::Error> {
+    let cmd_args: Vec<&str> = config["fate"]["command"].as_str().unwrap().split(" ").collect();
+    info!("Running pre-run command: {}", config["fate"]["pre_run_command"].as_str().unwrap());
+    let prerun: Vec<&str> = config["fate"]["pre_run_command"].as_str().unwrap().split(" ").collect();
+    let mut prerun_cmd = Command::new(&prerun[0]);
+    if prerun.len() > 1 {
+        prerun_cmd.args(&prerun[1..]);
+    }
+    let res = prerun_cmd.current_dir(run_dir).output()?;
+    let pre_run_output = format!("{}/{}.prerun.stdout", config["fate"]["result_directory"].as_str().unwrap(), commit_hash);
+    std::fs::write(pre_run_output, res.stdout)?;
+    let pre_run_output = format!("{}/{}.prerun.stderr", config["fate"]["result_directory"].as_str().unwrap(), commit_hash);
+    std::fs::write(pre_run_output, res.stderr)?;
+    let mut deferred_error: Option<Error> = None;
+    info!("Running build/test command: {}", config["fate"]["command"].as_str().unwrap());
+    let res = Command::new(cmd_args[0])
+        .args(&cmd_args[1..])
+        .env("FATE_SAMPLES", config["fate"]["samples_directory"].as_str().unwrap())
+        .current_dir(run_dir)
+        .output()
+        .unwrap();
+    let cmd_output = format!("{}/{}.cmd.stdout", config["fate"]["result_directory"].as_str().unwrap(), commit_hash);
+    std::fs::write(cmd_output, res.stdout)?;
+    let cmd_output = format!("{}/{}.cmd.stderr", config["fate"]["result_directory"].as_str().unwrap(), commit_hash);
+    std::fs::write(cmd_output, res.stderr)?;
+    if !res.status.success() {
+        return Err(Error::new(ErrorKind::Other, format!("Build/test process exited with error code: {}", res.status.code().unwrap())));
+    }
+    Ok(())
+}
+
 fn run_fate_test(base_repo: &str, config: &Yaml, commit_hash: git2::Oid) -> Result<bool, std::io::Error> {
     let new_repo_loc = format!("{}/{}", config["fate"]["tmp_directory"].as_str().unwrap(),commit_hash);
     if Path::exists(Path::new(&new_repo_loc)) {
@@ -90,31 +122,21 @@ fn run_fate_test(base_repo: &str, config: &Yaml, commit_hash: git2::Oid) -> Resu
     }
     info!("Cloning repo to {}", &new_repo_loc);
     let new_repo = git2::Repository::clone(base_repo, &new_repo_loc).unwrap();
+    defer!{
+        std::fs::remove_dir_all(Path::new(&new_repo_loc)).unwrap();
+    };
     match new_repo.set_head_detached(commit_hash) {
         Ok(k) => k,
         Err(e) => return Err(Error::new(ErrorKind::UnexpectedEof, format!("Could not set the repo to the correct state: {}", e.message()))),
     }
     info!("Head has been set detatched to commit {}", &commit_hash);
-    let cmd_args: Vec<&str> = config["fate"]["command"].as_str().unwrap().split(" ").collect();
-    //XXX: move to pre-run command
-    info!("Running pre-run command: {}", config["fate"]["pre_run_command"].as_str().unwrap());
-    let prerun: Vec<&str> = config["fate"]["pre_run_command"].as_str().unwrap().split(" ").collect();
-    let mut prerun_cmd = Command::new(&prerun[0]);
-    if prerun.len() > 1 {
-        prerun_cmd.args(&prerun[1..]);
-    }
-    prerun_cmd.current_dir(&new_repo_loc).output()?;
-    let mut deferred_error: Option<Error> = None;
-    info!("Running build/test command: {}", config["fate"]["command"].as_str().unwrap());
-    let res = Command::new(cmd_args[0])
-        .args(&cmd_args[1..])
-        .env("FATE_SAMPLES", config["fate"]["samples_directory"].as_str().unwrap())
-        .current_dir(&new_repo_loc)
-        .output()
-        .unwrap();
-    if !res.status.success() {
-        deferred_error = Some(Error::new(ErrorKind::Other, format!("Build/test process exited with error code: {}", res.status.code().unwrap())));
-    }
+    let mut deferred_error: Option<std::io::Error> = None;
+    let res = run_commands(&new_repo_loc, config, &commit_hash);
+    // Leaving Err for later because we may still want the fate results to this point.
+    match res {
+        Ok(_) => {},
+        Err(e) => deferred_error = Some(e),
+    };
     info!("Collecting results");
     let fate_results = collect_results(&new_repo_loc)?;
     info!("submitting results");
